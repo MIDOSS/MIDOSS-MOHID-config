7e46ba0e8cdc15f8bf3041ea0daddb5d0d5c50fc
Doug Latornell <dlatornell@eoas.ubc.ca>
2020-04-22 11:02:49 -0700

    Add template for Monte-Carlo GLOST task bash script

---
+++ monte-carlo/templates/glost-task.sh
@@ -0,0 +1,4 @@
+{{ make_hdf5_cmd }} $MONTE_CARLO/forcing-yaml/{{ job_id }}-make-hdf5-{{ run_number }}.yaml {{ start_yyyy_mm_dd }} {{ n_days }} \
+&& {{ mohid_cmd }} run --no-submit --tmp-run-dir $MONTE_CARLO/{{ job_id }}-{{ run_number }}/ \
+  $MONTE_CARLO/mohid-yaml/{{ job_id }}-{{ run_number }}.yaml $MONTE_CARLO/results/{{ job_id }}-{{ run_number }}/ \
+&& bash $MONTE_CARLO/{{ job_id }}-{{ run_number }}/MOHID.sh
